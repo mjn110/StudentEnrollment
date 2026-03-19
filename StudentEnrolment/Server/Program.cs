3c2b46f565034bbb6b244e400001855ec09973ec
mjn110 <57441281+mjn110@users.noreply.github.com>
2026-03-19 16:05:43 +0000

    Changing production environment to display exceptions.

--- StudentEnrolment/Server/Program.cs
+++ StudentEnrolment/Server/Program.cs
@@ -56,7 +56,8 @@ if (app.Environment.IsDevelopment())
 }
 else
 {
-    app.UseExceptionHandler("/Error");
+    app.UseWebAssemblyDebugging();
+    //app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.  
     app.UseHsts();
 }
